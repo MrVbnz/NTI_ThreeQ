c58e3b2226f173166882e0fe396d2d80e80117cb
MrVbnz <55917883+MrVbnz@users.noreply.github.com>
2020-04-17 11:30:21 +0500

    comments

--- code/main.py
+++ code/main.py
@@ -10,29 +10,36 @@ from sensor_msgs.msg import Image
 import numpy as np
 
 
+#перемещение по заданным координатам
 def navigate_wait(x, y, z):
     print navigate(x=x, y=y, z=z, speed=1, frame_id=frame_id_t)
     rospy.sleep(sleepTime)
 
-
+#получение и обработка изображения с камеры
 def image_callback(data):
     global needQRFlag
     global b_data
     global color_in_cam
+    
+    #получение изображения
     img = bridge.imgmsg_to_cv2(data, 'bgr8')
     hsv = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
 
+    #отбор двух границ красного
     red_1 = cv2.inRange(hsv, red_1_l, red_1_h)
     red_2 = cv2.inRange(hsv, red_2_l, red_2_h)
 
+    #отбор остальных цветов и обьедининие границ красного
     red = cv2.bitwise_or(red_1, red_2)
     yellow = cv2.inRange(hsv, yellow_l, yellow_h)
     green = cv2.inRange(hsv, green_l, green_h)
 
+    #подсчет количества пикселей нужноых цветов
     redInCam = red.sum()
     yellowInCam = yellow.sum()
     greenInCam = green.sum()
 
+    #выбор результата распознавания
     if redInCam > yellowInCam and redInCam > greenInCam:
         color_in_cam = "red"
     if yellowInCam > redInCam and yellowInCam > greenInCam:
@@ -40,9 +47,12 @@ def image_callback(data):
     if greenInCam > redInCam and greenInCam > yellowInCam:
         color_in_cam = "green"
 
+    #публикация цветовых масок в каналы отладки
     debug_r_pub.publish(bridge.cv2_to_imgmsg(red, 'bgr8'))
     debug_y_pub.publish(bridge.cv2_to_imgmsg(yellow, 'bgr8'))
     debug_g_pub.publish(bridge.cv2_to_imgmsg(green, 'bgr8'))
+    
+    #распознавание и печать QR
     if needQRFlag:
         mask = cv2.inRange(img, (0, 0, 0), (200, 200, 200))
         thresholded = cv2.cvtColor(mask, cv2.COLOR_GRAY2BGR)
@@ -51,7 +61,7 @@ def image_callback(data):
         b_data = barcodes.data.encode("utf-8")
         print 'QR data: ' + str(b_data)
 
-
+#определение цвета в заданных координатах
 def detect_color(x, y, i):
     print 'TryToDetectColor'
     navigate_wait(x, y, flyz - 0.6)
@@ -63,7 +73,7 @@ def detect_color(x, y, i):
         redIndex.append(i)
     rospy.sleep(5)
 
-
+#определение QR в заданных координатах
 def detect_QR(x, y):
     global needQRFlag
     print 'TryToReadQR'
@@ -78,7 +88,7 @@ def detect_QR(x, y):
         set_effect(r=0, g=0, b=0)  # blink with white color
     navigate_wait(x, y, flyz)
 
-
+#перемещение к зараженному по индексу
 def gotoreal_sick(listIndex):
     print 'GoesToReadQR'
     for i in range(len(listIndex)):
@@ -90,6 +100,7 @@ def gotoreal_sick(listIndex):
 
 rospy.init_node('flight')
 
+#заполнение HSV границ цветов для cv2.inRange
 trs = 15
 green_d = np.array((58, 31, 80))
 green_h = np.array((green_d[0] + trs, green_d[1] + trs, green_d[2] + trs))
@@ -111,10 +122,12 @@ redInCam = 0
 greenInCam = 0
 yellowInCam = 0
 
+#создание каналов публикации
 debug_r_pub = rospy.Publisher('~debug_color_r', Image)
 debug_y_pub = rospy.Publisher('~debug_color_y', Image)
 debug_g_pub = rospy.Publisher('~debug_color_g', Image)
 
+#создание каналов публикации
 get_telemetry = rospy.ServiceProxy('get_telemetry', srv.GetTelemetry)
 navigate = rospy.ServiceProxy('navigate', srv.Navigate)
 navigate_global = rospy.ServiceProxy('navigate_global', srv.NavigateGlobal)
@@ -125,9 +138,10 @@ set_rates = rospy.ServiceProxy('set_rates', srv.SetRates)
 land = rospy.ServiceProxy('land', Trigger)
 arming = rospy.ServiceProxy('mavros/cmd/arming', CommandBool)
 
+#подписка на камеру с низким FPS
 bridge = CvBridge()
 image_sub = rospy.Subscriber('/main_camera/image_raw_throttled', Image, image_callback, queue_size=1)
-rospy.spin()
+rospy.spin() #СТРОКА СКОПИРОВАНА ИЗ МАНУАЛА. 3 раза помешала успешному запуску.
 
 sickList = [[0, 2.27], [0.72, 3.94], [0.72, 1.5], [2.88, 1.5], [2.88, 3.94], [2.16, 0.28], [1.44, 2.72], [1.44, 1.5],
             [2.16, 2.72], [3.66, 0.28]]
@@ -144,7 +158,7 @@ needQRFlag = False
 b_data = ''
 color_in_cam = ''
 
-
+#взлет
 flyz=1.2
 print 'Try navigate body'
 print navigate(z=flyz, speed=0.5, frame_id='body', auto_arm=True)
@@ -155,6 +169,7 @@ rospy.sleep(10)
 
 startPoint = get_telemetry(frame_id=frame_id_t)
 
+#проход по известным точкам
 for i in range(len(sickList)):
     currentSickX = sickList[i][0]
     currentSickY = sickList[i][1]
@@ -163,10 +178,12 @@ for i in range(len(sickList)):
     navigate_wait(currentSickX, currentSickY, flyz)
     print 'DoneWithCurrentSick x: ' + str(currentSickX) + 'y: ' + str(currentSickY)
 
+#возврат в точку старта
 print 'FliesHome'
 print navigate(x=startPoint.x, y=startPoint.y, z=flyz, speed=1, frame_id=frame_id_t)
 rospy.sleep(6)
 
+#преземление + сон 2 мин
 print land()
 rospy.sleep(5)
 print arming(False)
@@ -174,6 +191,7 @@ print arming(False)
 print 'Wait 2 min'
 rospy.sleep(120)
 
+#второй взлет
 flyz=1.2
 print 'Try navigate body'
 print navigate(z=flyz, speed=0.5, frame_id='body', auto_arm=True)
@@ -182,15 +200,18 @@ print 'Try navigate aruco_map'
 print navigate(z=flyz, speed=0.5, frame_id='aruco_map', auto_arm=True)
 rospy.sleep(5)
 
+#посещение красных и желтых точек
 print redIndex
 print yellowIndex
 gotoreal_sick(redIndex)
 gotoreal_sick(yellowIndex)
 
+#возврат в точку старта
 print 'FliesHome'
 print navigate(x=startPoint.x, y=startPoint.y, z=flyz, speed=1, frame_id=frame_id_t)
 rospy.sleep(6)
 
+#преземление
 print land()
 rospy.sleep(5)
 print arming(False)
